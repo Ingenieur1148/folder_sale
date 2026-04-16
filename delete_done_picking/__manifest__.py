387cf2b1e0e562a9ead136a3571f4e012b19409d
objetsdesign <randa@objetsdesign.com>
2026-04-16 14:49:58 +0100

    add_new_code

--- delete_done_picking/__manifest__.py
+++ delete_done_picking/__manifest__.py
@@ -1,14 +1,21 @@
-# -*- coding: utf-8 -*-
 {
-    'name': 'Delete Done Picking',
-    'version': '1.0',
-    'summary': 'Allow deletion of done stock pickings (DANGEROUS)',
-    'author': 'Custom',
-    'category': 'Inventory',
-    'depends': ['stock'],
+    'name': 'Delete Done picking',
+    'version': '18.0.1.0.0',
+    'category': 'Stock/Stock',
+    'summary': 'User can delete stock done',
+    'description': 'Stock picking for Odoo 18',
+    'author': 'Randa Dev',
+    'website': '',
+    'license': 'OPL-1',
+    'price': 29.99,
+    'currency': 'EUR',
+    'depends': ['web', 'base', 'stock'],
     'data': [
         'views/stock_picking_view.xml',
     ],
+    'images': [
+        'description/banner.png',
+    ],
     'installable': True,
-    'application': False,
+    'application': True,
 }
